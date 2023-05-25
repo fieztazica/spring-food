58336caf0c79cd2cd64612c24c3afed35399507f
fiezt <dat09t@gmail.com>
2023-05-25 20:53:54 +0700

    init

---
+++ src/test/java/owlvernyte/springfood/SpringFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package owlvernyte.springfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFoodApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
